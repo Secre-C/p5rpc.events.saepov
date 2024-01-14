3b2fb6c1fa4c772224cdd2ef056ba2ff6c3225bf
SecreC <gabirocks505@gmail.com>
2024-01-13 20:58:51 -0800

    fixed an exception that triggers when resetting config.

--- Config.cs
+++ Config.cs
@@ -47,8 +47,8 @@ namespace p5rpc.events.saepov.Configuration
 
         [DisplayName("POV Model Major Id")]
         [Description("Allows the user to change which model POV will trigger for. THIS MAY NOT WORK CONSISTENLY FOR ALL MODELS.")]
-        [DefaultValue(1005)]
-        public short PovModelMajorId { get; set; } = 1005;
+        [DefaultValue((short)1005)]
+        public short PovModelMajorId { get; set; } = (short)1005;
     }
 
     /// <summary>
